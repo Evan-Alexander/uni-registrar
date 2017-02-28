3238e0311c22e714aec4f47ceba76da3f656affd
Jason Brown and Sarah Leahy <sarah@>
2017-02-28 15:07:38 -0800

    create twig for courses and students

--- app/app.php
+++ app/app.php
@@ -28,6 +28,35 @@ date_default_timezone_set('America/Los_Angeles');
         return $app['twig']->render('index.html.twig');
     });
 
+    $app->post("/courses", function() use ($app) {
+        $new_course = new Course($_POST['course_name'], $id = null);
+        $new_course->save();
+        return $app['twig']->render('show-courses.html.twig', array('courses' => Course::getAll()));
+    });
+
+    $app->get("/delete", function() use ($app) {
+        Course::getAll();
+        Course::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->post("/delete", function() use ($app) {
+        Course::getAll();
+        Course::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/add-students/{id}", function($id) use ($app) {
+        $course = Course::find($id);
+        return $app['twig']->render("show-students.html.twig", array('course' => $course, 'courses' => Course::getAll(), 'students'=> Student::getAll()));
+    });
+
+    $app->post("/student-list/{id}", function($id) use ($app) {
+        $course = Course::find($id);
+        $new_student = new Student($_POST['student-name'], $_POST['major'], null);
+        $new_student->save();
+        return $app['twig']->render("show-students.html.twig", array('course' => $course, 'courses' => Course::getAll(), 'students'=> Student::getAll()));
+    });
 
     return $app;
 ?>
